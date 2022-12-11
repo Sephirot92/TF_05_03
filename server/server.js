3cce04d2900b29965869966194841246aafe8781
Lukasz Czapraga <lczapraga@gmail.com>
2022-12-11 20:16:55 +0100

    0.0.2- Working copy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,56 @@ var cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 6053;
+const port = 5600;
 
+app.post('/calculateAdd', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    console.log("Adding started");
+    if(!Number.isInteger(Number(request.body.numberA)) || !Number.isInteger(Number(request.body.numberB))) {
+        response.json({message: 'Błąd danych. Try again'});
+    }
+
+    response.json({ result: Number(request.body.numberA) + Number(request.body.numberB) });
+})
+
+app.post('/calculateSubstract', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    console.log("substraction started");
+    if(!Number.isInteger(Number(request.body.numberA)) || !Number.isInteger(Number(request.body.numberB))) {
+        response.json({message: 'Błąd danych. Try again'});
+    }
+
+    response.json({ result: Number(request.body.numberA) - Number(request.body.numberB) });
+})
+
+app.post('/multiply', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    console.log("Multiplication started");
+    if(!Number.isInteger(Number(request.body.numberA)) || !Number.isInteger(Number(request.body.numberB))) {
+        response.json({message: 'Błąd danych. Try again'});
+    }
+
+    response.json({ result: Number(request.body.numberA) * Number(request.body.numberB) });
+})
+
+app.post('/divide', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    console.log("Dividing started");
+    if(!Number.isInteger(Number(request.body.numberA)) || !Number.isInteger(Number(request.body.numberB))) {
+        response.json({message: 'Błąd danych. Try again'});
+    }
+
+    response.json({ result: Number(request.body.numberA) / Number(request.body.numberB) });
+})
+
+app.post('/power', cors(), (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    console.log("Power started");
+    if(!Number.isInteger(Number(request.body.numberA)) || !Number.isInteger(Number(request.body.numberB))) {
+        response.json({message: 'Błąd danych. Try again'});
+    }
+
+    response.json({ result: Math.pow(Number(request.body.numberA), Number(request.body.numberB)) });
+})
 
 app.listen(port, () => console.log(`Aplikacja działa na porcie ${port}`));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,150 @@
 var ajaxService = null;
 
 function init() {
+    console.log("Init started")
+    document.getElementById("summarize").addEventListener("click", addingFunction);
+    document.getElementById("substraction").addEventListener("click", substractingFunction);
+    document.getElementById("multiplication").addEventListener("click", multiplicationFunction);
+    document.getElementById("division").addEventListener("click", divisionFunction);
+    document.getElementById("power").addEventListener("click", powerFunction);
     ajaxService = new AjaxService();
 }
+
+function addingFunction() {
+    var numberA = document.getElementById("first_number_input").value;
+    console.log(numberA);
+    
+    var numberB = document.getElementById("second_number_input").value;
+    console.log(numberB);
+    ajaxService.post({
+        url: 'http://127.0.0.1:5600/calculateAdd',
+        data: {
+            numberA: numberA,
+            numberB: numberB
+        },
+        success: function (response) {
+            var result = document.getElementById("third_number_input");
+            Object.entries(response).forEach(([key, value]) => {
+                if(key === "result") {
+                    result.value = response.result;
+                } else if (key === "message") {
+                    result.value = response.message;
+                }
+            })
+            
+            
+        }
+    });
+}
+
+function substractingFunction() {
+    var numberA = document.getElementById("first_number_input").value;
+    console.log(numberA);
+    
+    var numberB = document.getElementById("second_number_input").value;
+    console.log(numberB);
+    ajaxService.post({
+        url: 'http://127.0.0.1:5600/calculateSubstract',
+        data: {
+            numberA: numberA,
+            numberB: numberB
+        },
+        success: function (response) {
+            var result = document.getElementById("third_number_input");
+            Object.entries(response).forEach(([key, value]) => {
+                if(key === "result") {
+                    result.value = response.result;
+                } else if (key === "message") {
+                    result.value = response.message;
+                }
+            })
+            
+            
+        }
+    });
+}
+
+function multiplicationFunction() {
+    var numberA = document.getElementById("first_number_input").value;
+    console.log(numberA);
+    
+    var numberB = document.getElementById("second_number_input").value;
+    console.log(numberB);
+    ajaxService.post({
+        url: 'http://127.0.0.1:5600/multiply',
+        data: {
+            numberA: numberA,
+            numberB: numberB
+        },
+        success: function (response) {
+            var result = document.getElementById("third_number_input");
+            Object.entries(response).forEach(([key, value]) => {
+                if(key === "result") {
+                    result.value = response.result;
+                } else if (key === "message") {
+                    result.value = response.message;
+                }
+            })
+            
+            
+        }
+    });
+}
+
+function divisionFunction() {
+    var numberA = document.getElementById("first_number_input").value;
+    console.log(numberA);
+    
+    var numberB = document.getElementById("second_number_input").value;
+    console.log(numberB);
+    ajaxService.post({
+        url: 'http://127.0.0.1:5600/divide',
+        data: {
+            numberA: numberA,
+            numberB: numberB
+        },
+        success: function (response) {
+            var result = document.getElementById("third_number_input");
+            Object.entries(response).forEach(([key, value]) => {
+                if(key === "result") {
+                    result.value = response.result;
+                } else if (key === "message") {
+                    result.value = response.message;
+                }
+            })
+            
+            
+        }
+    });
+}
+
+function powerFunction() {
+    var numberA = document.getElementById("first_number_input").value;
+    console.log(numberA);
+    
+    var numberB = document.getElementById("second_number_input").value;
+    console.log(numberB);
+    ajaxService.post({
+        url: 'http://127.0.0.1:5600/power',
+        data: {
+            numberA: numberA,
+            numberB: numberB
+        },
+        success: function (response) {
+            var result = document.getElementById("third_number_input");
+            Object.entries(response).forEach(([key, value]) => {
+                if(key === "result") {
+                    result.value = response.result;
+                } else if (key === "message") {
+                    result.value = response.message;
+                }
+            })
+            
+            
+        }
+    });
+}
+
+
+
+document.addEventListener("DOMContentLoaded", init);
